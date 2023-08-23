440f0004a2923e3676eb9a4215faa12dafc55c75
Victor Huang <huang.victor.mail@gmail.com>
2023-08-23 16:50:55 +0200

    Problem with use client

--- shadow-case-study/app/page.tsx
+++ shadow-case-study/app/page.tsx
@@ -1,6 +1,24 @@
+'use client'
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useQuery } from "react-query"
 
 export default function Home() {
-  
+  const [productsList, setProductsList] = useState([])
+  const { data } = useQuery({
+    queryKey: ['data'],
+    queryFn: async () => {
+      const response = await axios.get('/api/products/all')
+      return response.data
+    },
+    onSuccess: (data) => {
+      setProductsList(data)
+    }
+  })
+  useEffect(()=>{
+    console.log(productsList)
+  }, [productsList])
   return (
     <>
       Salut
