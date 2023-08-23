e8a09f42392bd5f42140331bb9d89c1d47ce4c5e
Victor Huang <huang.victor.mail@gmail.com>
2023-08-23 17:40:09 +0200

    Setup Search Input field

--- shadow-case-study/components/providers/products-list-provider.tsx
+++ shadow-case-study/components/providers/products-list-provider.tsx
@@ -5,33 +5,57 @@ import axios from 'axios'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
+import { Input } from '../ui/input'
 
 const ProductsListProvider = () => {
-    const [productsList, setProductsList] = useState<Product[]>([])
-    const { data } = useQuery({
-      queryKey: ['list'],
-      queryFn: async () => {
-        const response = await axios.get('/api/product/list')
-        return response.data
-      },
-      onSuccess: (data) => {
-        setProductsList(data)
-      }
-    })
-    useEffect(()=>{
-      console.log(productsList)
-    }, [productsList])
+  const [productsList, setProductsList] = useState<Product[]>([])
+  const [searchValue, setSearchValue] = useState<string>("")
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
+
+
+  const { isError } = useQuery({
+    queryKey: ['list'],
+    queryFn: async () => {
+      const response = await axios.get('/api/product/list')
+      return response.data
+    },
+    onSuccess: (data) => {
+      setProductsList(data)
+    }
+  })
+
+  useEffect(()=>{
+    setFilteredProducts(
+      productsList.filter((product) =>
+          product.brandName?.toLowerCase().includes(searchValue.toLowerCase()) ||
+          product.displayName?.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    )
+    console.log(productsList)
+  }, [productsList, searchValue])
+
+  if(isError){
+    return(
+        <p>Error with product List</p>
+    )
+  }
   return (
     <>
-        <ul className="space-y-6">
-            {productsList.map((product, index) => (
-                <li key={index} className="bg-red-200">
-                    <Link href={`/product/${product.productId}`}>
-                        {product?.brandName}
-                    </Link>
-                </li>
-            ))}
-        </ul>
+      <Input
+        placeholder="Search"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <ul className="space-y-6">
+          {filteredProducts.map((product, index) => (
+              <li key={index} className="bg-red-200">
+                  <Link href={`/product/${product.productId}`}>
+                      {product?.brandName}
+                      {product?.displayName}
+                  </Link>
+              </li>
+          ))}
+      </ul>
     </>
   )
 }
