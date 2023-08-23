d407d8714729ce54dc016634bfa04778a244c35c
Victor Huang <huang.victor.mail@gmail.com>
2023-08-23 16:59:13 +0200

    Fix Url problem

--- shadow-case-study/app/page.tsx
+++ shadow-case-study/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       Test
-      {/* <ProductsListProvider /> */}
+      <ProductsListProvider />
     </>
   )
 }
--- shadow-case-study/components/providers/products-list-provider.tsx
+++ shadow-case-study/components/providers/products-list-provider.tsx
@@ -5,20 +5,20 @@ import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 
 const ProductsListProvider = () => {
-    // const [productsList, setProductsList] = useState([])
-    // const { data } = useQuery({
-    //   queryKey: ['data'],
-    //   queryFn: async () => {
-    //     const response = await axios.get('/api/products/all')
-    //     return response.data
-    //   },
-    //   onSuccess: (data) => {
-    //     setProductsList(data)
-    //   }
-    // })
-    // useEffect(()=>{
-    //   console.log(productsList)
-    // }, [productsList])
+    const [productsList, setProductsList] = useState([])
+    const { data } = useQuery({
+      queryKey: ['data'],
+      queryFn: async () => {
+        const response = await axios.get('/api/products/list')
+        return response.data
+      },
+      onSuccess: (data) => {
+        setProductsList(data)
+      }
+    })
+    useEffect(()=>{
+      console.log(productsList)
+    }, [productsList])
   return (
     <div>
       HEY
